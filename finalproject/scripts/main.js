6e360b98e311bb8d97da3e16086984de8ea7a7f6
24on7store <132136928+24on7store@users.noreply.github.com>
2025-06-20 10:46:09 +0300

    update

--- finalproject/scripts/main.js
+++ finalproject/scripts/main.js
@@ -2,7 +2,24 @@
 const data = [
   { name: "Casual Leather", price: "$80", color: "Brown", image: "images/casual1.webp" },
   { name: "Formal Black", price: "$95", color: "Black", image: "images/formal1.webp" },
-  { name: "White Sneaker", price: "$75", color: "White", image: "images/sneakers1.webp" }
+  { name: "White Sneaker", price: "$75", color: "White", image: "images/sneakers1.webp" },
+
+  { name: "Casual Leather", price: "$99.99", color: "Brown", image: "images/casual2.webp" },
+  { name: "Formal Black", price: "$59.99", color: "Black", image: "images/formal2.webp" },
+  { name: "White Sneaker", price: "$75.99", color: "White", image: "images/sport2.webp" },
+
+  { name: "Casual Leather", price: "$40", color: "Brown", image: "images/casual3.webp" },
+  { name: "Formal Black", price: "$45", color: "Black", image: "images/formal3.webp" },
+  { name: "White Sneaker", price: "$105", color: "White", image: "images/sport3.webp" },
+
+  { name: "Casual Leather", price: "$60", color: "Brown", image: "images/casual4.webp" },
+  { name: "Formal Black", price: "$45", color: "Black", image: "images/formal4.webp" },
+  { name: "White Sneaker", price: "$65", color: "White", image: "images/sport4.webp" },
+  
+
+  { name: "Casual Leather", price: "$70", color: "Brown", image: "images/casual5.webp" },
+  { name: "Formal Black", price: "$85", color: "Black", image: "images/formal5.webp" },
+  { name: "White Sneaker", price: "$55", color: "White", image: "images/sport5.webp" }
 ];
 
 const container = document.getElementById("dynamic-container");
@@ -41,4 +58,5 @@ copyrightParagraph.innerHTML = + currentYear;
 var lastModifiedParagraph = document.getElementById('lastModified');
 var lastModifiedDate = document.lastModified;
 lastModifiedParagraph.innerHTML = 'Last Modification: ' + lastModifiedDate;
+
 
